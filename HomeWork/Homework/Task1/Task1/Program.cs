4f08605e127a66e47e2dc7b9840da502b0f22d11
sys.peshkov <sys.peshkov@gmail.com>
2022-11-25 08:09:35 +0300

    Fix task1

--- HomeWork/Homework/Task1/Task1/Program.cs
+++ HomeWork/Homework/Task1/Task1/Program.cs
@@ -6,28 +6,27 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
-Console.Clear;
 Console.WriteLine("Введите число a");
-int a = int.Parse(ReadLine());
+int a = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите число b");
-int b = int.Parse(ReadLine());
+int b = int.Parse(Console.ReadLine());
 
 if (a > b) 
 {
    Console.WriteLine("Число a больше b");
-   Console.Write("a =");
+   Console.Write("a = ");
    Console.WriteLine(a);
 }
 else if (a < b) 
 {
    Console.WriteLine("Число b больше a");
-   Console.Write("b =");
+   Console.Write("b = ");
    Console.WriteLine(b);
 }
 else 
 {
     Console.WriteLine("Число b равно a");
-    Console.Write("b =");
+    Console.Write("b = ");
     Console.WriteLine(b);
 }
