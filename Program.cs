095e9400a41aa5d8c29884b42e5b03d98c1c6a1c
sys.peshkov <sys.peshkov@gmail.com>
2022-11-25 07:16:10 +0300

    init project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int number  = int.Parse(Console.ReadLine());
+
+int quarter = number*number;
+Console.WriteLine(quarter);
