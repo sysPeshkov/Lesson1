8180f73349b4db2cf9cec7070c9943f20d507030
sys.peshkov <sys.peshkov@gmail.com>
2022-11-25 07:33:03 +0300

    Done Task2

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b*b )
+{
+Console.WriteLine("Да. a квадрат числа b");
+
+}
+else
+{
+Console.WriteLine("Нет. a Не квадрат числа b");
+
+}
