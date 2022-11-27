5445eb81b2955b150ae583a3590fd9d0be9ca36e
sys.peshkov <sys.peshkov@gmail.com>
2022-11-27 20:03:31 +0300

    Done Examle004

--- Example004/Example004/Program.cs
+++ Example004/Example004/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Random random = new Random();
+int number = random.Next(10, 100);
+
+int firstNumber = number / 100;
+int lastNumber = number % 10;
+
+int result = firstNumber + ;
+
+if (lastNumber > max ) {
+    max = lastNumber;
+}
+
+Console.WriteLine(number);
+Console.WriteLine(firstNumber);
+Console.WriteLine(lastNumber);
+Console.Write("Из них максимальное = ");
+Console.WriteLine(max);
