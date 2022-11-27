88007de980b28a9f31c308dc1484d5ad6c1976d3
sys.peshkov <sys.peshkov@gmail.com>
2022-11-27 20:14:27 +0300

    Done example005 about 2 number

--- Example005/Example005/Program.cs
+++ Example005/Example005/Program.cs
@@ -1,18 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Random random = new Random();
-int number = random.Next(10, 100);
+int number = random.Next(100, 1000);
 
 int firstNumber = number / 100;
 int lastNumber = number % 10;
-
-int result = firstNumber + ;
-
-if (lastNumber > max ) {
-    max = lastNumber;
-}
-
+int Result =firstNumber*10+lastNumber;
 Console.WriteLine(number);
-Console.WriteLine(firstNumber);
-Console.WriteLine(lastNumber);
-Console.Write("Из них максимальное = ");
-Console.WriteLine(max);
+Console.Write(Result);
+
+
+int debt = lastNumber % firstNumber;
+if ( debt == 0) {
+Console.WriteLine(debt);
+}
