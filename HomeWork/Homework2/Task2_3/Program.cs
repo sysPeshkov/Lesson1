365e432eefbded5981aed0366f2d1242a0b29edf
sys.peshkov <sys.peshkov@gmail.com>
2022-11-28 08:05:43 +0300

    Done HomeWork Task2_3

--- HomeWork/Homework2/Task2_3/Program.cs
+++ HomeWork/Homework2/Task2_3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+Console.Clear();
+Console.WriteLine("Введите номер дня недели ");
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isParsed) {
+    Console.WriteLine("Пожалуйста, введите целое число ");
+    return;
+}
+
+if (number == 6 | number == 7){
+    Console.WriteLine("Да ");
+    return;
+}
+else {
+    Console.WriteLine("Нет ");
+    return;
+}
