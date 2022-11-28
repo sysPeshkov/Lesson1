772b4dd17491ccf9e412be32f59939e71fb7e1fd
sys.peshkov <sys.peshkov@gmail.com>
2022-11-28 07:41:49 +0300

    Done Homework Task2_1

---
+++ HomeWork/Homework2/Task2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+Console.Clear();
+Console.WriteLine("Введите трёхзначное целое число ");
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isParsed |  100 > number | number > 999) {
+    Console.WriteLine("Пожалуйста, введите трёхзначное целое число ");
+    return;
+}
+
+int result = (number / 10)%10;
+
+Console.WriteLine (result);
