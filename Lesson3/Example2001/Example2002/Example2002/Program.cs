7ae5d0cab25583594797c2b9c3fadc785ca75c28
sys.peshkov <sys.peshkov@gmail.com>
2022-11-30 07:54:28 +0300

    Done Example2002

--- Lesson3/Example2001/Example2002/Example2002/Program.cs
+++ Lesson3/Example2001/Example2002/Example2002/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Решение в группах задач:
+Задача 18: Напишите программу, которая по заданному номеру
+четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+Console.WriteLine("Введите номе четверти");
+bool isParsedQuarterNumber = int.TryParse(Console.ReadLine(), out int quarterNumber);
+
+
+string diapozon = GetQuarterNumberByCoordinates(quarterNumber);
+
+
+Console.Write("Вам доступен диапозон - ");
+Console.WriteLine(diapozon);
+
+
+string GetQuarterNumberByCoordinates(int quarterNumber) {
+    string a;
+    if (quarterNumber == 1) {
+        a = "x > 0 && y > 0";
+        return a;
+    }
+        if (quarterNumber == 2) {
+        a = "x < 0 && y > 0";
+        return a;
+    }
+        if (quarterNumber == 3) {
+        a = "x < 0 && y < 0"   ;
+        return a;
+    }
+        if (quarterNumber == 4) {
+            a = "x > 0 && y < 0";
+        return a;
+    }
+
+    return "-1";
+}
