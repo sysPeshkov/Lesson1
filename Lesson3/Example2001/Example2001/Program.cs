9d10a6085ced5233e0911680232b026357e0357f
sys.peshkov <sys.peshkov@gmail.com>
2022-11-30 07:26:29 +0300

    Done example2001

--- Lesson3/Example2001/Example2001/Program.cs
+++ Lesson3/Example2001/Example2001/Program.cs
@@ -10,3 +10,30 @@ if (!isParsedX || !isParsedY || x == 0 || y == 0)
 Console.WriteLine("Вы ввели некорректные параметры");
 
 }
+
+int quarterNumber = GetQuarterNumberByCoordinates(x, y);
+
+if (quarterNumber == -1) {
+    Console.WriteLine("Вы ввели некорректные параметры");
+    return;
+}
+
+Console.Write("Четверть номер ");
+Console.WriteLine(quarterNumber);
+
+int GetQuarterNumberByCoordinates(int x, int y) {
+    if (x > 0 && y > 0) {
+        return 1;
+    }
+        if (x < 0 && y > 0) {
+        return 2;
+    }
+        if (x < 0 && y < 0) {
+        return 3;
+    }
+        if (x > 0 && y < 0) {
+        return 4;
+    }
+
+    return -1;
+}
