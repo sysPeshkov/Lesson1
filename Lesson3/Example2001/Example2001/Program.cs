89e97862009ddf5988d49a10dcb4758acd606a36
sys.peshkov <sys.peshkov@gmail.com>
2022-11-30 07:18:58 +0300

    Create Lesson2

---
+++ Lesson3/Example2001/Example2001/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите x");
+bool isParsedX = int.TryParse(Console.ReadLine(), out int x);
+Console.WriteLine("Введите y");
+bool isParsedY = int.TryParse(Console.ReadLine(), out int y);
+
+
+if (!isParsedX || !isParsedY || x == 0 || y == 0)
+{
+Console.WriteLine("Вы ввели некорректные параметры");
+
+}
