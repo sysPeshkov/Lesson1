77993d9ea5a7b7b232913d55f0df0754129650e6
sys.peshkov <sys.peshkov@gmail.com>
2022-12-04 07:45:40 +0300

    Done Task4_3

--- Lesson4/Task4_3/Program.cs
+++ Lesson4/Task4_3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] num = GenerateArray();
+PrintArray(num);
+
+void PrintArray(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]}");
+    }
+    
+    }
+
+int[] GenerateArray(){
+int[] result = new int[8];
+Random rand = new Random();
+for (int i = 0; i < result.Length; i++) {
+    result[i] = rand.Next(0,2);
+
+}
+return result;
+}
