25d2be3243c77c259dc85bdefbcad395e2c4efa6
sys.peshkov <sys.peshkov@gmail.com>
2022-12-09 07:01:09 +0300

    Done HW5_Task1

---
+++ HomeWork/HomeWork5/HW5_Task1/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+Tests();
+//int min = InputNumber("Введите нижнюю границу ");
+int min = 100;
+//int max = InputNumber("Введите верхнюю границу ");
+int max = 1000;
+int size = InputNumber("Введите размер массива ");
+int[] array = new int[size];
+array = GenerateArray(min, max, size);
+PrintArray(array);
+
+int num = GetCountOfEvenNumbers(array);
+Console.WriteLine($"Количество четных чисел = {num}");
+
+
+int GetCountOfEvenNumbers(int[] array){
+int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count+=1;
+        }
+    }
+return count;
+}
+
+
+
+int[] GenerateArray(int min, int max, int size){
+    int[] resultArray = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = random.Next(min, max+1);
+    }
+    return resultArray;
+
+}
+
+
+int InputNumber(string name){
+    Console.WriteLine(name);
+    string number = Console.ReadLine();
+    bool isParsed = int.TryParse(number, out int result);
+
+    if (!isParsed) {
+        Console.WriteLine($"Вы ввели некорректное число, ожидается целое число, вы ввели {number}");
+        return -1;
+    }
+    return result;
+}
+
+void PrintArray(int[] array){
+    int length = array.Length;
+    Console.Write("[ ");
+for (int i = 0; i < length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+    Console.Write("]");
+    Console.WriteLine("");
+}
+
+void Tests() {
+int[] input = {345, 897, 568, 234};
+int expectedResult = 2;
+ if (GetCountOfEvenNumbers(input) != expectedResult)
+ { 
+        Console.WriteLine($"Error. Expect = {expectedResult}, result =  {GetCountOfEvenNumbers(input)}");
+      
+ }
+}
---
+++ Lesson5/Task5_4/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве. [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+Console.Clear();
+
+int a = InputNumber("Введите длину массива ");
+int[] array = new int[a];
+array = FillArray(a);
+
+PrintArray(array);
+int[] arrayResult = new int[a/2];
+arrayResult = Multiplication(array, a/2);
+PrintArray(arrayResult);
+
+int[] Multiplication(int[] array, int size){
+    int[] result = new int[size];
+    int min = 0;
+    int max = array.Length-1;
+    while (min < max)
+    {
+        result[min] = array[min] * array[max];
+        min += 1;
+        max -= 1;
+    }
+    return result;
+}
+
+
+
+int[] FillArray(int length){
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+    //Console.Write($"Введите элемент массива {i+1}");
+    resultArray[i] = InputNumber($"Введите элемент массива {i+1}");
+    }
+    return resultArray;
+}
+
+int InputNumber(string name){
+    Console.WriteLine(name);
+    string number = Console.ReadLine();
+    bool isParsed = int.TryParse(number, out int result);
+
+    if (!isParsed) {
+        Console.WriteLine($"Вы ввели некорректное число, ожидается целое число, вы ввели {number}");
+        return -1;
+    }
+    return result;
+}
+
+void PrintArray(int[] array){
+    int length = array.Length;
+    Console.Write("[ ");
+for (int i = 0; i < length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+    Console.Write("]");
+    Console.WriteLine("");
+}
