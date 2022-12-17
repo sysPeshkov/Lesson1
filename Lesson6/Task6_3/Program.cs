f7e5cdc8c88ee0732db23cf62f8cdbfa097973d0
sys.peshkov <sys.peshkov@gmail.com>
2022-12-17 08:12:37 +0300

    Done Task6_3

--- Lesson6/Task6_3/Program.cs
+++ Lesson6/Task6_3/Program.cs
@@ -5,4 +5,30 @@
 // 5 92 3
 // 8 42 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
-Console.WriteLine("Hello, World!");
+using static Major.Class1.Helper;
+
+int m = InputNumber("Введите количество строк");
+int n = InputNumber("Введите количество столбцов");
+
+int[,] array = Fill2DArray(m, n);
+Console.WriteLine("Вы задали массив:");
+Print2DArray(array);
+
+int sum = GetSumDiagonalElement(array);
+Console.WriteLine($"Результат задания: {sum}");
+
+int GetSumDiagonalElement(int[,] array)
+{
+int sumDiagonalElement = 0;
+ for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == j){
+                        sumDiagonalElement += array[i,j]; 
+                    }
+                }
+
+            }
+            return sumDiagonalElement;
+}
