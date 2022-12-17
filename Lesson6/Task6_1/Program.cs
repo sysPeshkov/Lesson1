c1d549d15801d9b49c6e5fd04c4dec09dddc79eb
sys.peshkov <sys.peshkov@gmail.com>
2022-12-17 07:30:17 +0300

    Done Task6_1

---
+++ Lesson6/Task6_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₙₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+using static Major.Class1.Helper;
+
+int m = InputNumber("Введите количество строк");
+int n = InputNumber("Введите количество столбцов");
+
+int[,] array = Generate2DArray(m, n);
+
+Print2DArray(array);
+
+
+
+int[,] Generate2DArray(int m, int n)
+{
+
+    int[,] resultArray = new int[m, n];
+    for (int i = 0; i < resultArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultArray.GetLength(1); j++)
+        {
+            resultArray[i, j] = i + j;
+        }
+
+    }
+    return resultArray;
+
+}
--- Major/Class1.cs
+++ Major/Class1.cs
@@ -1,6 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-namespace Common;
-
+﻿namespace Major;
+public class Class1
+{
     public static class Helper
     {
         public static int InputNumber(string name)
@@ -16,4 +16,18 @@ namespace Common;
             }
             return result;
         }
+        public static void Print2DArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                   Console.Write($"{array[i,j]}  ");
+                }
+            Console.WriteLine("");
+            }
+        }
+
+
     }
+}
