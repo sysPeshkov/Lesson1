d755bc86fb9cc942d83e37730e22c856a391bd90
sys.peshkov <sys.peshkov@gmail.com>
2022-12-21 08:03:48 +0300

    Create HomeWork8

---
+++ HomeWork/HomeWork8/HW8_Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 54: Задайте двумерный массив.
+//  Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+using static Major.Class1.Helper;
+Console.Clear();
+
+// int m = InputNumber("Введите количество строк");
+// int n = InputNumber("Введите количество столбцов");
+// int min = InputNumber("Введите минимальное значение");
+// int max = InputNumber("Введите максимальное");
+
+// int[,] array = Generate2DArray(m, n, min, max);
+// Console.WriteLine("Вы задали массив:");
+// Print2DArray(array);
+
+int[] arr = { 8, 2, 5, 6, 2, 1 };
+PrintArrayInt(SortNumberInArray(arr));
+
+int[] SortNumberInArray(int[] array)
+{
+
+    int length = array.Length;
+    int min = array[0];
+    int tmp = 0;
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = array[i];
+    }
+    for (int i = 1; i < length - 1; i++)
+    {
+        if (resultArray[i] < min)
+        {
+            tmp = resultArray[i - 1];
+            resultArray[i - 1] = resultArray[i];
+            resultArray[i] = tmp;
+        }
+    }
+    return resultArray;
+
+}
+
+
+
+
+
+
---
+++ Lesson8/Task8_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// Набор данных
+// Частотный массив
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+// 0 встречается 2 раза 
+// 1 встречается 1 раз
+//  2 встречается 1 раз 
+//  8 встречается 1 раз 
+//  9 встречается 3 раза
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+
+using static Major.Class1.Helper;
+Console.Clear();
+
+int m = InputNumber("Введите количество строк");
+int n = InputNumber("Введите количество столбцов");
+
+int[,] array = Fill2DArray(m, n);
+Print2DArray(array);
+
+void GetSequense(int[,] array)
+{
+    int countRows = array.GetLength(0);
+    int countColumns = array.GetLength(1);
+    int countUnicNumbers = 0;
+    int tmp = 0;
+
+    for (int i = 0; i < countRows; i++)
+    {
+        for (int j = 0; j < countColumns; j++)
+        {
+           if (array[i,j] == tmp){
+            
+           }
+        }
+    }
+
+}
