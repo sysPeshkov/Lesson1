143b77dca60487b2410abc47920ef7d45294deb2
sys.peshkov <sys.peshkov@gmail.com>
2022-12-27 20:03:57 +0300

    Done HW9_Task1

---
+++ HomeWork/HomeWork9/HW9_Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+//числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using static Major.Class1.Helper;
+Console.Clear();
+
+int n = InputNumber("Введите N");
+if (n <= 0) {
+    Console.WriteLine("Введите положительное целое число больше нуля");
+    n = 1;
+}
+ Console.WriteLine(" "); 
+GetNumbersFromNToOne(n);
+
+void GetNumbersFromNToOne(int n){
+
+    if (n == 0)
+    {
+     return;
+    }
+ Console.Write($"{n} "); 
+ GetNumbersFromNToOne(n-1);
+
+}
