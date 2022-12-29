fd2aa876efcc393fee4d0b20c6d37a264c974747
sys.peshkov <sys.peshkov@gmail.com>
2022-12-29 08:17:05 +0300

    Done HW9_Task2

--- HomeWork/HomeWork9/HW9_Task2/Program.cs
+++ HomeWork/HomeWork9/HW9_Task2/Program.cs
@@ -7,6 +7,33 @@
 using static Major.Class1.Helper;
 Console.Clear();
 
-int rows = InputNumber("Введите количество строк");
-int columns = InputNumber("Введите количество столбцов");
-int high = InputNumber("Введите размерность 3-го измерения");
+int m = InputNumber("Введите M");
+int n = InputNumber("Введите N. Число должно быть больше M");
+
+if (n <= 0 || m <= 0) {
+    Console.WriteLine("Введите положительное целое число больше нуля");
+    n = 2;
+    m = 1;
+}
+if (n <= m) {
+    Console.WriteLine("N должно быть больше M");
+    n = 2;
+    m = 1;
+}
+ Console.WriteLine(" "); 
+
+int sum = GetSumFromMToN(m, n);
+Console.WriteLine(sum);
+
+int GetSumFromMToN(int m, int n){
+int sum = 0;
+    if (m == n)
+    {
+     return 1;
+    }
+
+sum = n + GetSumFromMToN(m, n-1);
+
+return sum;
+}
+
